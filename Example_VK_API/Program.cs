81c411f8a8609dbd8a97e89b4e2dc7384a514eb5
tanyabednyakova <tmbednyakova@gmail.com>
2021-07-04 20:46:18 +0300

    добавить получение списка подписчиков

--- Example_VK_API/Program.cs
+++ Example_VK_API/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.IO;
+using System.Text;
 using VkNet;
 using VkNet.Model;
 using VkNet.Model.RequestParams;
@@ -9,7 +10,7 @@ namespace Example_VK_API
     class Program
     {
         // получить токен из файла
-        public static string getAuth()
+        public static string getAuthForGroup()
         {
             string fileName = @"auth_vk.txt";
             string token = "";
@@ -27,6 +28,26 @@ namespace Example_VK_API
             return token;
         }
 
+        // получить токен из файла
+        public static string getAuthForUser()
+        {
+            string fileName = @"auth_vk.txt";
+            string token = "";
+            try
+            {
+                using (StreamReader sr = new StreamReader(fileName))
+                {
+                    sr.ReadLine();
+                    token = sr.ReadLine();
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            return token;
+        }
+
         static void Main(string[] args)
         {
 
@@ -34,7 +55,7 @@ namespace Example_VK_API
             // обработать исключения!
             api_group.Authorize(new ApiAuthParams
             {
-                AccessToken = getAuth()
+                AccessToken = getAuthForGroup()
             });
 
             // получить список подписчиков сообщества (для сообщества)
@@ -47,14 +68,21 @@ namespace Example_VK_API
             // получит запись со стены (для пользователя)
             var api_user = new VkApi();
             // обработать исключения!
-            api_group.Authorize(new ApiAuthParams
+            api_user.Authorize(new ApiAuthParams
             {
-                AccessToken = getAuth()
+                AccessToken = getAuthForUser()
             });
             var get = api_user.Wall.Get(new WallGetParams());
             foreach (var wallPosts in get.WallPosts)
-                Console.WriteLine(wallPosts.Text);
-  
+                Console.WriteLine(Encoding.Default.GetString(Encoding.UTF8.GetBytes(wallPosts.Text)));
+
+            //var groups = api.Groups.Get(new VkNet.Model.RequestParams.GroupsGetParams() { UserId = getFollowers[0].Id });
+            //foreach (var g in groups)
+            //{
+
+            //}
+
+
 
 
 
